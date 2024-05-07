45890394a85e0fde3ce78d33696be397d1374597
gp0987gp <gp0987gp@gmail.com>
2024-05-07 15:43:26 -0300

    att

--- database/migrations/2024_05_07_183057_create_personal_access_tokens_table.php
+++ database/migrations/2024_05_07_183057_create_personal_access_tokens_table.php
@@ -11,8 +11,12 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('muvinns', function (Blueprint $table) {
+        Schema::create('servicos', function (Blueprint $table) {
             $table->id();
+            $table->string('nome', 100)->unique()->nullable(false);
+            $table->string('descricao', 200)->nullable(false);
+            $table->integer('duracao', )->nullable(false);
+            $table->decimal ('preco', )->nullable(false);
             $table->timestamps();
         });
     }
